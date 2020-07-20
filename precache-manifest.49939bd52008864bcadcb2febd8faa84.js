5ba0b0274d4722aefff248f7899d84285ef5d8e6
Sourav Rakshit <38202655+souravrax@users.noreply.github.com>
2020-07-21 01:45:35 +0530

    Updates

---
+++ precache-manifest.49939bd52008864bcadcb2febd8faa84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b35c90e918e2b148eb1656abee07a4",
+    "url": "/souravrax.github.io/todo-app/index.html"
+  },
+  {
+    "revision": "aca2d4a974b8292200dc",
+    "url": "/souravrax.github.io/todo-app/static/css/main.03706253.chunk.css"
+  },
+  {
+    "revision": "e387b6176b90248c7d94",
+    "url": "/souravrax.github.io/todo-app/static/js/2.e40ccce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/souravrax.github.io/todo-app/static/js/2.e40ccce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca2d4a974b8292200dc",
+    "url": "/souravrax.github.io/todo-app/static/js/main.0cca4e46.chunk.js"
+  },
+  {
+    "revision": "50492a8d50a13b0f19a6",
+    "url": "/souravrax.github.io/todo-app/static/js/runtime-main.0a2fcb84.js"
+  }
+]);
